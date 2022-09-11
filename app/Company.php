4441a09a7e3d9371c95b3e9821797abd17d0c8c7
KenYoshii <racisa.kenken@gmail.com>
2022-09-11 23:51:44 +0900

    リレーション作成

--- app/Company.php
+++ app/Company.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    //リレーション
+    public function products(){
+        return $this->hasMany('App\Models\Product');
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    //リレーション
+    public function company(){
+        return $this->belongsTo('App\Models\Companies');
+    }
+
+    public function sales(){
+        return $this->hasMany('App\Models\Sale');
+    }
+
 }
