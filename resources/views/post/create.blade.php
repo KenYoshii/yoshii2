04616cd2271f6bd30510ca24b8682aa9163cf2d8
KenYoshii <racisa.kenken@gmail.com>
2022-09-12 02:11:49 +0900

    CRUD controller作成

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+	<div class="col-md-10 mt-6">
+		<div class="card-body">
+			<h1 class="mt4">新規登録</h1>
+			<form enctype="multipart/form-data">
+				<div class="form-group">
+					<label for="title">商品名</label>
+					<input type="text" name="title" class="form-control" id="title" placeholder="Enter Title">
+				</div>
+
+				<div class="form-group">
+					<label for="body">本文</label>
+					<textarea name="body" class="form-control" id="body" cols="30" rows="10"><textarea>
+				</div>
+
+				<div class="form-group">
+					<label for="image">画像</label>
+					<div calss="col-md-6">
+						<input id="image" type="file" name="image">
+				</div>
+			</div>
+
+				<button type="submit" class="btn-success">送信する</button>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
